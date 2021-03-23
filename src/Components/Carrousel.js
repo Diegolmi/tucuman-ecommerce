e28c8b7df9eacd7157123138b961fa7fc77f1f3f
martin jerez <martincho111986@gmail.com>
2021-03-23 11:15:44 -0300

    cambios en varias secciones y se agrego modal

--- src/Components/Carrousel.js
+++ src/Components/Carrousel.js
@@ -1,42 +1,35 @@
-import React from 'react'
-import carousel1 from '../assets/img/carusel1ecommerce.png';
-import carousel2 from '../assets/img/carusel2ecommerce.png';
-import carousel3 from '../assets/img/carusel3ecommerce.png';
+import React from "react";
+import carousel1 from "../assets/img/carusel1ecommerce.png";
+import carousel2 from "../assets/img/carusel2ecommerce.png";
+import carousel3 from "../assets/img/carusel3ecommerce.png";
 
-
-import { Carousel } from 'antd';
-
-function onChange(a, b, c) {
-  console.log(a, b, c);
-}
+import { Carousel } from "antd";
 
 const contentStyle = {
- width:'100%',
-  
+  width: "100%",
 };
 
 const Carrousel = () => {
-    return (
-        <>
-        <div className="containerCarusel"  >
+  return (
+    <>
+      <div className="containerCarusel">
+        <Carousel autoplay>
+          <div>
+            <img src={carousel1} style={contentStyle} alt="" />
+          </div>
+          <div>
+            <img src={carousel2} style={contentStyle} alt="" />
+          </div>
+          <div>
+            <img src={carousel3} style={contentStyle} alt="" />
+          </div>
+          <div>
+            <img src={carousel1} style={contentStyle} alt="" />
+          </div>
+        </Carousel>
+      </div>
+    </>
+  );
+};
 
-        <Carousel autoplay afterChange={onChange}>
-    <div>
-     <img src={carousel1}  style={contentStyle} alt=""/>
-    </div>
-    <div>
-    <img src={carousel2}  style={contentStyle} alt=""/>
-    </div>
-    <div>
-    <img src={carousel3}  style={contentStyle}  alt=""/>
-    </div>
-    <div>
-    <img src={carousel1}  style={contentStyle} alt=""/>
-    </div>
-  </Carousel>
-        </div>
-        </>
-    )
-}
-
-export default Carrousel
+export default Carrousel;
